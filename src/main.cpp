9c9386df5641a3a49c634a796fe3439b8d4c89eb
blfuentes <epiphonetodie@gmail.com>
2024-08-08 21:49:32 +0200

    added default

--- src/main.cpp
+++ src/main.cpp
@@ -10,9 +10,16 @@ extern "C" void app_main();
 
 static const char *TAG = "example";
 
+/* Use project configuration menu (idf.py menuconfig) to choose the GPIO to blink,
+   or you can edit the following line and set a number here.
+*/
 #define BLINK_GPIO GPIO_NUM_8
+#define CONFIG_BLINK_LED_STRIP 1
+#define CONFIG_BLINK_LED_STRIP_BACKEND_SPI 1
 
 static uint8_t s_led_state = 0;
+
+#ifdef CONFIG_BLINK_LED_STRIP
 
 static led_strip_handle_t led_strip;
 
@@ -38,15 +45,42 @@ static void configure_led(void)
         .strip_gpio_num = BLINK_GPIO,
         .max_leds = 1, // at least one LED on board
     };
+#if CONFIG_BLINK_LED_STRIP_BACKEND_RMT
+    led_strip_rmt_config_t rmt_config = {
+        .resolution_hz = 10 * 1000 * 1000, // 10MHz
+        .flags.with_dma = false,
+    };
+    ESP_ERROR_CHECK(led_strip_new_rmt_device(&strip_config, &rmt_config, &led_strip));
+#elif CONFIG_BLINK_LED_STRIP_BACKEND_SPI
     led_strip_spi_config_t spi_config = {
         .spi_bus = SPI2_HOST
     };
     spi_config.flags.with_dma = true;
     ESP_ERROR_CHECK(led_strip_new_spi_device(&strip_config, &spi_config, &led_strip));
-
+#else
+#endif
     /* Set all LED off to clear all pixels */
     led_strip_clear(led_strip);
 }
+
+#elif CONFIG_BLINK_LED_GPIO
+
+static void blink_led(void)
+{
+    /* Set the GPIO level according to the state (LOW or HIGH)*/
+    gpio_set_level(BLINK_GPIO, s_led_state);
+}
+
+static void configure_led(void)
+{
+    ESP_LOGI(TAG, "Example configured to blink GPIO LED!");
+    gpio_reset_pin(BLINK_GPIO);
+    /* Set the GPIO as a push/pull output */
+    gpio_set_direction(BLINK_GPIO, GPIO_MODE_OUTPUT);
+}
+
+#else
+#endif
 
 // Define task handle
 TaskHandle_t Task1;
